3f9d3d664701762f0e5ea6edbc121b44b5609339
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-06-16 16:37:00 -0700

    Update Push

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,34 @@
+var Family = "The Arizzinis", Dad = "Jeremiah", Mom = "Hermoine",
+	Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+document.write(3+3);
+
+function My_First_Function() {				//Defining a function and naming it
+	var str = "This text is green!";		//Defining a variable and giving it a 
+											//string value
+	var result = str.fontcolor("green");	//Using the fontcolor method on
+											//str variable
+	document.getElementById("Green_Text").innerHTML = result;  //Putting the value
+																//of result into HTML element with "Green_Text" id
+}
+
+function My_Second_Function() {
+	var str = "This text is red!";
+	
+	var result = str.fontcolor("red");
+	
+	document.getElementById("Red_Text").innerHTML = result;
+}
+
+	
+
+		
+		
+		
+		
+		
