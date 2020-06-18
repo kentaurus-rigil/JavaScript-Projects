e6d431c2e676e2b33a46605ddec797a81cc9f3ef
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-06-17 20:30:35 -0700

    Add New Doc

---
+++ JS/Basic_JavaScript_5.js
@@ -0,0 +1,110 @@
+document.write(typeof "word");
+
+document.write("<br>");
+
+document.write(typeof 3);
+
+document.write("<br>");
+
+
+function my_Function() {
+	document.getElementById("Test").innerHTML = isNaN('This is a string');
+}
+
+function apple() {
+	document.getElementById("Tests").innerHTML = isNaN('007');
+}
+
+document.write("<br>");
+
+document.write(2E310);
+
+document.write("<br>");
+
+document.write(-3E310);
+
+document.write("<br>");
+
+document.write(10>2);
+
+document.write("<br>");
+
+document.write(10<2);
+
+console.log(2 + 2);
+
+document.write("<br>");
+
+document.write("10" + 5);
+
+document.write("<br>");
+
+document.write(100<5);
+
+console.log(8>90);
+
+document.write("<br>");
+
+document.write(10==10);
+
+document.write("<br>");
+
+document.write(3==11);
+
+document.write("<br>");
+
+a = 10;
+b = 10;
+document.write(a === b);
+
+document.write("<br>");
+
+x = 82;
+y = "82";
+document.write(x === y);
+
+document.write("<br>");
+
+c = 13;
+d = 14;
+document.write(c === d);
+
+document.write("<br>");
+
+e = 20;
+f = "30";
+document.write(e === f);
+
+document.write("<br>");
+
+document.write(5 > 2 && 10 > 4);
+
+document.write("<br>");
+
+document.write(5 > 10 && 10 > 4);
+
+document.write("<br>");
+
+document.write(5 > 10 || 10 > 4);
+
+document.write("<br>");
+
+document.write(5 > 10 || 10 > 20);
+
+document.write("<br>");
+
+function not_Function() {
+	document.getElementById("Not").innerHTML = !(20 > 10);
+}
+
+
+function not_Functions() {
+	document.getElementById("Not_1").innerHTML = !(5 > 10);
+}
+
+
+
+
+
+
+
